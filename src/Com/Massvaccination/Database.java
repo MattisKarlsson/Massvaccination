a87d452a3377f4bd39b5fb408ab920bb29fc0d7c
Mattis Karlsson <72206178+MattisKarlsson@users.noreply.github.com>
2021-02-25 16:55:09 +0100

    Worked further on the Database

---
+++ src/Com/Massvaccination/Database.java
@@ -0,0 +1,32 @@
+package Com.Massvaccination;
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class Database {
+
+    public boolean createDatabase(String filename) {
+        try{
+            File myObj = new File(filename);
+            return  myObj.createNewFile();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean updateDatabase(String filename, String country, byte months) {
+
+        try
+            FileWriter myWriter = new FileWriter(filename);
+            myWriter.write(country);
+            myWriter.write(months);
+            myWriter.close();
+            return true;
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
